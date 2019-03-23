b7cd39f8ecba547f84acb379e294d6a439bbc1e1
root <root@Eldhose.localdomain>
2019-03-23 13:56:30 +0530

    Basic Working Model

--- backend/server.js
+++ backend/server.js
@@ -13,18 +13,18 @@ app.get('/', function (req, res) {
 //--------------------------------------------------------------------------------------------------
 app.get('/get_coin', function (req, res) {
     console.log("Connected to :-> /get_coin");
-    request('http://10.90.90.79:5000/get_chain',function(err1,res1,body1){
+    request('http://192.168.225.156:5000/get_chain',function(err1,res1,body1){
 		if(!err1){
-			console.log("HTTP Request to 'http://10.90.90.79:5000/get_chain' -> Okay");
+			console.log("HTTP Request to 'http://10.90.101.216:5000/get_chain' -> Okay");
 			res.end(body1);
 		}
 		else{
-			console.log("HTTP Request to 'http://10.90.90.79:5000/get_chain' -> Not Okay");
+			console.log("HTTP Request to 'http://10.90.101.216:5000/get_chain' -> Not Okay");
 			res.end("ERROR");
 		}
 	});
 });
 //--------------------------------------------------------------------------------------------------
-var server = app.listen(3000, function () {
-    console.log("Server Started at port 3000 !.......");
+var server = app.listen(80, function () {
+    console.log("Server Started at port 80 !.......");
 });
